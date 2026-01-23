6f4a3b326e20bc6f1b009096ba5aa6057e63e66b
Herbert Tamayo <hftamayo@gmail.com>
2026-01-23 10:02:28 -0600

    project starting point

---
+++ src/main/java/com/hftamayo/absencesbobe/AbsencesbobeApplication.java
@@ -0,0 +1,13 @@
+package com.hftamayo.absencesbobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsencesbobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsencesbobeApplication.class, args);
+	}
+
+}
