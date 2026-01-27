caa16db1fd68371511df3e2488785f17b79e523c
Herbert Tamayo <hftamayo@gmail.com>
2026-01-27 09:32:08 -0600

    updates in create and update company requests for idempotency based operations

---
+++ src/main/java/com/hftamayo/absencesbobe/features/companies/adapters/web/dto/UpdateCompanyRequest.java
@@ -0,0 +1,19 @@
+package com.hftamayo.absencesbobe.features.companies.adapters.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UpdateCompanyRequest(
+        @NotBlank
+        @Size(min = 2, max = 50)
+        String name,
+
+        @NotBlank
+        @Size(min = 5, max = 200)
+        String description,
+
+        @NotBlank
+        @Size(min = 5, max = 100)
+        String address
+) {
+}
