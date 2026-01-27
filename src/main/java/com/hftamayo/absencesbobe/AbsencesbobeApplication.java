f7e42ae7d06931e9a63fe8507dd542f4f051ebcd
Herbert Tamayo <hftamayo@gmail.com>
2026-01-27 08:45:26 -0600

    starting point for main application

--- src/main/java/com/hftamayo/absencesbobe/AbsencesbobeApplication.java
+++ src/main/java/com/hftamayo/absencesbobe/AbsencesbobeApplication.java
@@ -1,13 +1,32 @@
 package com.hftamayo.absencesbobe;
 
+import com.sun.tools.javac.Main;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+@Import(com.hftamayo.absencesbobe.shared.web.jackson.JacksonTrimmingConfig.class)
+@EnableConfigurationProperties
+@RequiredArgsConstructor
+
 public class AbsencesbobeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AbsencesbobeApplication.class, args);
+		SpringApplication.run(Main.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void applicationReady() {
+		System.out.println("\n========================================================");
+		System.out.println("        The project is up and running!");
+//        System.out.println("        Version: " + com.hftamayo.java.todo.utilities.version.VersionConstants.getFullVersion());
+//        System.out.println("        API Version: " + com.hftamayo.java.todo.utilities.version.VersionConstants.getCurrentApiVersion());
+		System.out.println("========================================================\n");
 	}
 
 }
