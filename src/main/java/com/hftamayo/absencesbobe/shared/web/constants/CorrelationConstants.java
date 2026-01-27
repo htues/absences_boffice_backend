25f187ff7c04a566ec4d5a84da39a421476f8d85
Herbert Tamayo <hftamayo@gmail.com>
2026-01-27 12:52:42 -0600

    using response and correlation constants

---
+++ src/main/java/com/hftamayo/absencesbobe/shared/web/constants/CorrelationConstants.java
@@ -0,0 +1,8 @@
+package com.hftamayo.absencesbobe.shared.web.constants;
+
+public final class CorrelationConstants {
+    public static final String HEADER = "X-Correlation-Id";
+    public static final String ATTRIBUTE = "correlationId";
+
+    private CorrelationConstants() {}
+}
