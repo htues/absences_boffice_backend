2c9359b12f8b6b2adf3420ae36ce125595d7d8ae
Herbert Tamayo <hftamayo@gmail.com>
2026-01-29 10:02:58 -0600

    renaming to codeDescriptor

---
+++ src/main/java/com/hftamayo/absencesbobe/shared/web/constants/CodeDescriptor.java
@@ -0,0 +1,7 @@
+package com.hftamayo.absencesbobe.shared.web.constants;
+
+public interface CodeDescriptor {
+    String getResponseType();
+    int getStatusCode();
+    String getMessageKey();
+}
--- src/main/java/com/hftamayo/absencesbobe/shared/web/response/ApiResponseDescriptor.java
+++
@@ -1,7 +0,0 @@
-package com.hftamayo.absencesbobe.shared.web.response;
-
-public interface ApiResponseDescriptor {
-    String getResponseType();
-    int getStatusCode();
-    String getMessageKey();
-}
