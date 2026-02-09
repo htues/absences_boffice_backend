3adfa65662e191bea1f543337f7271adbddcc6ba
Herbert Tamayo <hftamayo@gmail.com>
2026-02-09 15:01:42 -0600

    deprecating custom data migration method

--- src/main/java/com/hftamayo/absencesbobe/shared/infrastructure/seed/SeedConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.hftamayo.absencesbobe.shared.infrastructure.seed;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(SeedProperties.class)
-public class SeedConfiguration {
-}
