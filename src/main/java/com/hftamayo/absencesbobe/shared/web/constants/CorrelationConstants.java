d807d76912e85b5a6ac941610deb089a90630f87
Herbert Tamayo <hftamayo@gmail.com>
2026-02-20 14:37:24 -0600

    using noArgsConstructor for CorrelationConstants

--- src/main/java/com/hftamayo/absencesbobe/shared/web/constants/CorrelationConstants.java
+++ src/main/java/com/hftamayo/absencesbobe/shared/web/constants/CorrelationConstants.java
@@ -1,8 +1,10 @@
 package com.hftamayo.absencesbobe.shared.web.constants;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public final class CorrelationConstants {
     public static final String HEADER = "X-Correlation-Id";
     public static final String ATTRIBUTE = "correlationId";
 
-    private CorrelationConstants() {}
 }
