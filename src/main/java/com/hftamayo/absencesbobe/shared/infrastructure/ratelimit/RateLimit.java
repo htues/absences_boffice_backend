7f7b7ad45398a38ae9e899085f7c25e49b839508
Herbert Tamayo <hftamayo@gmail.com>
2026-05-20 15:36:01 -0600

    adjusting ratelimit interface

--- src/main/java/com/hftamayo/absencesbobe/shared/infrastructure/ratelimit/RateLimit.java
+++ src/main/java/com/hftamayo/absencesbobe/shared/infrastructure/ratelimit/RateLimit.java
@@ -1,3 +1,5 @@
+package com.hftamayo.absencesbobe.shared.infrastructure.ratelimit;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
